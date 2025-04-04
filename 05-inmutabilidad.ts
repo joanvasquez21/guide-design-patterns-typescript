ff8c8f67930fac0e3cba3661d428de4eaeeaab42
joanvasquez21 <joanvrivas.21@gmail.com>
2025-04-04 23:40:30 +0200

    feat: added method main pattern inmutibility

--- 05-inmutabilidad.ts
+++ 05-inmutabilidad.ts
@@ -1,8 +1,72 @@
 /**
  * inmutabilidad con copia
- * aunque la inmutabilidad 
+ * aunque la inmutabilidad
+ * inmutable no podemos cambiar
  */
 class CodeEditorState{
-    readonly content: string
-    readonly cursosPosition: number;
+    readonly content: string;
+    readonly cursorPosition: number;
+    readonly unsaveChanges: boolean;
+
+    constructor(content: string, cursorPosition: number, unsaveChange: boolean ){
+        this.content   = content
+        this.cursorPosition = cursorPosition
+        this.unsaveChanges = unsaveChange
+    }
+
+    displayState(){
+        console.log('estado del editor:')
+        console.log(`
+            contenido: ${this.content}
+            cursor position: ${this.cursorPosition}
+            unsave change: ${this.unsaveChanges}
+            `)
+    }
+
+    copywith({content, cursorPosition, unsaveChanges}: Partial<CodeEditorState>): CodeEditorState{
+        return new CodeEditorState(
+            content ?? this.content,
+            cursorPosition ?? this.cursorPosition,
+            unsaveChanges ?? this.unsaveChanges
+        )
+    }
 }
+
+
+class CodeEditorHistory{
+
+    //metodo privado
+    private history: CodeEditorState[] = []
+    private currentIndex: number = -1; //
+
+    save(state: CodeEditorState): void{
+        if(this.currentIndex < this.history.length - 1 ){
+            this.history = this.history.slice(0, this.currentIndex +1)
+        }
+        
+        this.history.push(state)
+        this.currentIndex++
+    }
+
+    redo(): CodeEditorState | null{
+        if( this.currentIndex < this.history.length -1  ){
+            this.currentIndex++
+            return this.history[this.currentIndex]
+        }
+
+        return null;
+    }
+
+}
+
+function mainInmutability(){
+    const history = new CodeEditorHistory();
+    let editorState = new CodeEditorState(
+        "console.log('hello world');", 2, false);
+
+        history.save(editorState)
+
+        console.log('initial state')
+    }
+
+mainInmutability()
